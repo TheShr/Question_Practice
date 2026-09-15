581c71682fdb2229dea3457b906e541ff244492c
anuj <anujsharma214692@gmail.com>
2026-09-15 14:00:09 +0530

    Time: 0 ms (100%), Space: 15.4 MB (51.44%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -4,7 +4,6 @@ public:
         if(idx == 0){
             if(nums[0] == 0 && s2 == 0) return 2;
             if(nums[0] == s2 || s2 == 0) return 1;
-            
             return 0;
         }
 
@@ -19,6 +18,24 @@ public:
         int sum = accumulate(nums.begin(), nums.end(), 0);
         int s2 = (sum-target)/2;
         if((sum-target) % 2 != 0|| sum - target < 0) return 0;
-        return func(n-1, s2, nums);
+        
+        vector<vector<int>> dp(n, vector<int> (s2+1, 0));
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+
+        if(nums[0] <= s2 && nums[0] != 0) dp[0][nums[0]] = 1;
+        
+
+
+        for(int i = 1; i<n; i++){
+            for(int j = 0; j<= s2; j++){
+                int pick = 0;
+                if(nums[i] <= j) pick = dp[i-1][j-nums[i]];
+                int notpick = dp[i-1][j];
+
+                dp[i][j] = (pick + notpick);
+            }
+        }
+        return dp[n-1][s2];
     }
 };
