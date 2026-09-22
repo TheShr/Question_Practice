4fd03f00fadfbeaef2af12819b74578225fda819
anuj <anujsharma214692@gmail.com>
2026-09-22 20:10:14 +0530

    Time: 0 ms (100%), Space: 18.7 MB (10.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,45 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        queue<pair<int,int>> q;
+    void bfs(vector<vector<int>>& ans, vector<vector<int>>& vis, vector<vector<int>>& image, int sr, int sc, int color){
+        vis[sr][sc] = 1;
+        ans[sr][sc] = color;
         int n = image.size();
         int m = image[0].size();
-        int initial = image[sr][sc];
-        if(initial == color) return image;
+        queue<pair<int,int>> q;
 
         q.push({sr,sc});
-        image[sr][sc] = color;
-        int drow[] = {-1 , 0, +1, 0};
-        int dcol[] = {0 , 1 , 0 , -1};
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
         while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
-            for(int i = 0 ; i<4; i++){
-                int nrow = r + drow[i];
-                int ncol = c + dcol[i];
-                if(nrow < n && nrow >= 0 && ncol < m && ncol >= 0 && image[nrow][ncol] == initial){
-                    q.push({nrow,ncol});
-                    image[nrow][ncol] = color;
+
+            for(int k = 0; k<4; k++){
+                int nrow = row + dr[k];
+                int ncol = col + dc[k];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == image[sr][sc] && !vis[nrow][ncol]){
+                    vis[nrow][ncol] = 1;
+                    ans[nrow][ncol] = color;
+                    q.push({nrow, ncol});
                 }
             }
         }
-        return image;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        if(image.empty() || image[0].empty()) return image;
+        if(color == image[sr][sc]) return image;
+
+        vector<vector<int>> ans = image;
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        //ans = image;
+
+        bfs(ans, vis, image, sr, sc, color);
+        return ans;
     }
 };
