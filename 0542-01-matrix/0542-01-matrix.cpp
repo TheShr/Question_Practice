cbabd4a5bd7997646028a5806610dc50c559550c
anuj <anujsharma214692@gmail.com>
2026-09-23 15:18:35 +0530

    Time: 20 ms (48.72%), Space: 38.9 MB (29.46%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -13,7 +13,10 @@ public:
 
         for(int i =0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(mat[i][j] == 0 && !vis[i][j])  q.push({{i,j}, 0});
+                if(mat[i][j] == 0 && !vis[i][j]){
+                    q.push({{i,j}, 0});
+                    vis[i][j] = 1;
+                }
             }
         }
        
