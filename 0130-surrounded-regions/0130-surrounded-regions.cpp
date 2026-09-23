032ad63300237a728b5fd83e891514d00259c748
anuj <anujsharma214692@gmail.com>
2026-09-23 15:53:15 +0530

    Time: 3 ms (21.36%), Space: 15.1 MB (23.02%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,52 +1,48 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>> &vis,vector<vector<char>>& board){
-        int n = board.size();
-        int m = board[0].size();
-        vis[row][col] = 1;
-        int r[] = {-1, 0, 1 , 0};
-        int c[] = {0 , 1, 0, -1};
-        for(int i = 0; i<4; i++){
-            int nrow = row + r[i];
-            int ncol = col + c[i];
-            if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
-                dfs(nrow, ncol , vis , board);
-            }
-        }
-    }
     void solve(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-        vector<vector<int>> vis(n, vector(m,0));
-        for(int i =0; i<n; i++){
-            if(!vis[i][0] && board[i][0] == 'O'){
-                dfs(i,0,vis,board);
-            }
-            if(!vis[i][m-1] && board[i][m-1] == 'O'){
-                dfs(i, m-1, vis,board);
-            }
-        }
-        for(int j = 0; j<m; j++){
-            if(!vis[0][j] && board[0][j] == 'O'){
-                dfs(0, j, vis,board);
-            }
-            if(!vis[n-1][j] && board[n-1][j] == 'O'){
-                dfs(n-1, j, vis,board);
-            }
-        }
+        int m = board.size();
+        int n = board[0].size();
 
-        for(int i = 0; i<n; i++){
-            for(int j =0 ; j<m; j++){
-                if(!vis[i][j] && board[i][j] == 'O'){
-                    board[i][j] = 'X';
+        vector<vector<int>> vis(m, vector<int> (n, 0));
+        queue<pair<int, int>> q;
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i == 0 || j == 0 || i == m-1 || j == n-1){
+                   if(!vis[i][j] && board[i][j] == 'O'){
+                        vis[i][j] = 1;
+                        q.push({i, j});
+                    }
                 }
             }
         }
+
+        int dr[] = {0, 0, 1, -1};
+        int dc[] = {1, -1, 0, 0};
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            
+
+            for(int k = 0; k<4; k++){
+                int nrow = row + dr[k];
+                int ncol = col + dc[k];
+
+                if(nrow >= 0 && ncol >= 0 && nrow < m && ncol < n && !vis[nrow][ncol] && board[nrow][ncol] == 'O'){
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+
+        }
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(vis[i][j] == 0) board[i][j] = 'X';
+            }
+        }
+        
     }
 };
-/* 
-first we will traverse bondaries for 'O's mark as visited in vis
-
-then add condition we will traverse whole matrix if not vis mark 'X' 
-
-*/
